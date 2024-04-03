1f9b2d3118eac563b9d661d268cc0c1da59129b9
ArdrinRexhepi <ardrin1rexhepi@gmail.com>
2024-04-03 04:01:52 +0200

    show participants button

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -2,6 +2,7 @@ import { cn } from "@/lib/utils";
 import {
   CallControls,
   CallParticipantsList,
+  CallStatsButton,
   PaginatedGridLayout,
   SpeakerLayout,
 } from "@stream-io/video-react-sdk";
@@ -14,7 +15,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { LayoutList } from "lucide-react";
+import { LayoutList, Users } from "lucide-react";
 
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
@@ -40,7 +41,7 @@ const MeetingRoom = () => {
           <CallLayout />
         </div>
         <div
-          className={cn("h-[calc(100vh-86px)] ml-2", {
+          className={cn("h-[calc(100vh-86px)] ml-2 hidden", {
             "show-block": showParticipants,
           })}
         >
@@ -73,6 +74,13 @@ const MeetingRoom = () => {
             ))}
           </DropdownMenuContent>
         </DropdownMenu>
+        <CallStatsButton />
+
+        <button onClick={() => setShowParticipants((prev) => !prev)}>
+          <div className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+            <Users size={20} className="text-white" />
+          </div>
+        </button>
       </div>
     </section>
   );
