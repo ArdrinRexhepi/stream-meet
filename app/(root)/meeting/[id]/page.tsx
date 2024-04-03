2db0cdb403b509a65ee5d28019b520573fc632a1
ArdrinRexhepi <ardrin1rexhepi@gmail.com>
2024-04-03 03:32:20 +0200

    added meeting waitroom/setup

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -10,14 +10,18 @@ import React, { useState } from "react";
 const Meeting = ({ params: { id } }: { params: { id: string } }) => {
   const { user, isLoaded } = useUser();
   const { call, isCallLoading } = useGetCallById(id);
-  const [isSetupComplete, setIsSetupComplete] = useState();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
 
   if (!isLoaded || isCallLoading) return <Loader />;
   return (
     <main className="h-screen w-full">
       <StreamCall call={call}>
         <StreamTheme>
-          {!isSetupComplete ? <MeetingSetup /> : <MeetingRoom />}
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
         </StreamTheme>
       </StreamCall>
     </main>
--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,7 +1,69 @@
-import React from "react";
+"use client";
+import {
+  DeviceSettings,
+  VideoPreview,
+  useCall,
+} from "@stream-io/video-react-sdk";
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+const MeetingSetup = ({
+  setIsSetupComplete,
+}: {
+  setIsSetupComplete: (value: boolean) => void;
+}) => {
+  const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false);
 
-const MeetingSetup = () => {
-  return <div>MeetingSetup</div>;
+  const call = useCall();
+  useEffect(() => {
+    if (isMicCamToggledOn) {
+      call?.camera.disable();
+      call?.microphone.disable();
+    } else {
+      call?.camera.enable();
+      call?.microphone.enable();
+    }
+  }, [isMicCamToggledOn, call?.camera, call?.microphone]);
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+      <h1 className="text-2xl font-bold">Meeting Setup</h1>
+      <VideoPreview />
+      <div className="flex h-16 items-center justify-center gap-3">
+        {/* <label className="flex items-center justify-center gap-2 font-medium">
+          <input
+            type="checkbox"
+            checked={isMicCamToggledOn}
+            onChange={(e) => setIsMicCamToggledOn(e.target.checked)}
+          />
+          
+          Join with mic and camera off
+        </label> */}
+        <div className="flex items-center space-x-2">
+          <Checkbox
+            checked={isMicCamToggledOn}
+            onCheckedChange={() => setIsMicCamToggledOn((prev) => !prev)}
+          />
+          <label
+            htmlFor="terms"
+            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            Join with mic and camera off
+          </label>
+        </div>
+        <DeviceSettings />
+
+        <Button
+          className="rounded-md bg-green-500 px-4"
+          onClick={() => {
+            call?.join();
+            setIsSetupComplete(true);
+          }}
+        >
+          Join meeting
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default MeetingSetup;
