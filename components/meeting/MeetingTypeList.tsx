2fd82afa6923587afb4a0239c10eba3e4696de25
ArdrinRexhepi <ardrin1rexhepi@gmail.com>
2024-04-04 04:02:00 +0200

    completed home cards functionality

--- components/meeting/MeetingTypeList.tsx
+++ components/meeting/MeetingTypeList.tsx
@@ -8,6 +8,7 @@ import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 import { toast } from "sonner";
 import { Textarea } from "../ui/textarea";
 import ReactDatePicker from "react-datepicker";
+import { Input } from "../ui/input";
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -81,7 +82,7 @@ const MeetingTypeList = () => {
       />
       <HomeCard
         img="/icons/recordings.svg"
-        title="New Recordings"
+        title="View Recordings"
         description="Checkout your recordings"
         handleClick={() => router.push("/recordings")}
         className="bg-purple-1"
@@ -152,6 +153,20 @@ const MeetingTypeList = () => {
         buttonText="Start Meeting"
         handleClick={createMeeting}
       />
+      <MeetingModal
+        isOpen={meetingState === "isJoiningMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Type the link here"
+        className="text-center"
+        buttonText="Join Meeting"
+        handleClick={() => router.push(values.link)}
+      >
+        <Input
+          placeholder="Meeting link"
+          className="border-none bg-dark-3 focus-visible:ring-0"
+          onChange={(e) => setValues({ ...values, link: e.target.value })}
+        />
+      </MeetingModal>
     </section>
   );
 };
