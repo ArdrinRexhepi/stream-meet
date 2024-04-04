449d33dd6fb60ff4de1cbc1e582f650cc1cd0c24
ArdrinRexhepi <ardrin1rexhepi@gmail.com>
2024-04-04 02:04:20 +0200

    added ability to see recordings

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -2,9 +2,10 @@
 import { useGetCalls } from "@/hooks/useGetCalls";
 import { Call, CallRecording } from "@stream-io/video-react-sdk";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MeetingCard from "./MeetingCard";
 import Loader from "./layout/Loader";
+import { toast } from "sonner";
 
 const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
   const { endedCalls, upcomingCalls, callRecordings, isLoading } =
@@ -40,10 +41,29 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
     }
   };
 
-  const calls = getCalls();
-  const noCallsMessage = getNoCallsMessage();
+  useEffect(() => {
+    const fetchRecordings = async () => {
+      try {
+        const callData = await Promise.all(
+          callRecordings?.map((meeting) => meeting.queryRecordings()) ?? []
+        );
+
+        const recordings = callData
+          .filter((call) => call.recordings.length > 0)
+          .flatMap((call) => call.recordings);
+
+        setRecordings(recordings);
+      } catch (error) {
+        toast.warning("Try again later!");
+      }
+    };
+    if (type === "recordings") fetchRecordings();
+  }, [type, callRecordings]);
 
   if (isLoading) return <Loader />;
+
+  const calls = getCalls();
+  const noCallsMessage = getNoCallsMessage();
 
   return (
     <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
@@ -56,14 +76,15 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
                 ? "/icons/previous.svg"
                 : type === "upcoming"
                 ? "/icons/upcoming.svg"
-                : "/icons/recording.svg"
+                : "/icons/recordings.svg"
             }
             title={
-              (meeting as Call).state.custom.description.substring(0, 35) ||
+              (meeting as Call).state?.custom.description.substring(0, 35) ||
+              (meeting as CallRecording).filename.substring(0, 30) ||
               "No description"
             }
             date={
-              (meeting as Call).state.startedAt?.toLocaleString() ||
+              (meeting as Call).state?.startedAt?.toLocaleString() ||
               (meeting as CallRecording).start_time?.toLocaleString()
             }
             isPreviousMeeting={type === "ended"}
